fa1b31716b30fa980a36ae26f838d3d5ad56d2bb
Ong Wen Kai <ongwenkai92@gmail.com>
2022-02-24 17:48:07 +0800

    implement drag and drop

--- src/components/SelectCourse.js
+++ src/components/SelectCourse.js
@@ -1,19 +1,19 @@
-import './SelectCourse.css'
-import React from 'react';
-import { useDrop } from 'react-dnd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import "./SelectCourse.css";
+import React from "react";
+import { useDrop } from "react-dnd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SelectCourse(props) {
-  const [, drop] = useDrop(() => ({
-    accept: 'course',
-  }))
-
   return (
     <div className="SelectCourse">
       <div className="contentHeader">
         <div className="searchBox">
-          <input type="text" className="input__search" placeholder="Search"></input>
+          <input
+            type="text"
+            className="input__search"
+            placeholder="Search"
+          ></input>
           <button className="btn__search">
             <FontAwesomeIcon
               icon={faMagnifyingGlass}
@@ -22,7 +22,7 @@ function SelectCourse(props) {
           </button>
         </div>
         <div className="title">Courses</div>
-        <div className='blank'></div>
+        <div className="blank"></div>
       </div>
 
       <div className="coursetable">
@@ -32,19 +32,36 @@ function SelectCourse(props) {
           <div className="header__major2">Major 2</div>
         </div>
         <div className="courseBox">
-          <div ref={drop} className="box__core">
-            {props.setBoxForCourse("box__core")}
-          </div>
-          <div ref={drop} className="box__major1">
-            {props.setBoxForCourse("box__major1")}
-          </div>
-          <div ref={drop} className="box__major2">
-            {props.setBoxForCourse("box__major2")}
-          </div>
+          <CourseBox
+            setBoxForCourse={props.setBoxForCourse}
+            type="core"
+            onDrop={props.onDrop}
+          ></CourseBox>
+          <CourseBox
+            setBoxForCourse={props.setBoxForCourse}
+            type="major1"
+            onDrop={props.onDrop}
+          ></CourseBox>
+          <CourseBox
+            type="major2"
+            setBoxForCourse={props.setBoxForCourse}
+            onDrop={props.onDrop}
+          ></CourseBox>
         </div>
       </div>
     </div>
   );
 }
 
+function CourseBox({ type, onDrop, setBoxForCourse }) {
+  const [, drop] = useDrop(() => ({
+    accept: "course",
+    drop: (item) => onDrop(item.name, `box__${type}`),
+  }));
+  return (
+    <div ref={drop} className={`box__${type}`}>
+      {setBoxForCourse(`box__${type}`)}
+    </div>
+  );
+}
 export default SelectCourse;
