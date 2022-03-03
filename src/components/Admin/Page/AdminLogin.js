d1010f736d1b934164dcd1669fbd78a52c08e7dd
edbertrussel <65524467+edbertrussel@users.noreply.github.com>
2022-03-03 14:39:41 +0800

    Update AdminLogin.js

--- src/components/Admin/Page/AdminLogin.js
+++ src/components/Admin/Page/AdminLogin.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { text } from '@fortawesome/fontawesome-svg-core';
 //import HttpRequest from "../../../HttpRequest";
 
 
@@ -18,19 +19,48 @@ function AdminLogin() {
     });
   }; */
 
-  const Login = () => {
+  function validateForm() {
+    return text.length > 0 && password.length > 0;
+  }
+
+/*   async function Login(event) {
+    event.preventDeafault();
+
+    try {
+      await axios.post("http://localhost:5000/api/admin/login", {
+        adminId: adminId,
+        password: password
+      }).then( res => {
+        alert(res.data.status);
+    })
+    } catch (e) {
+      alert(e.message);
+    }
+  }  */
+
+/*   async function Login () {
     axios.post("http://localhost:5000/api/admin/login", {
       adminId: adminId,
       password: password
     }).then( res => {
         alert(res.data.status);
-        alert(res.data.message);
-        //setLoginStatus(res.data.status);
+    })
+    .catch( error => {
+      alert(error.message);
+    })
+  }; */
+
+    const Login = () => {
+    axios.post("http://localhost:5000/api/admin/login", {
+      adminId: adminId,
+      password: password
+    }).then( res => {
+        alert(res.data.status);
     })
     .catch( error => {
       alert(error);
     })
-  };
+  }; 
 
   return (
     <form>
@@ -39,18 +69,17 @@ function AdminLogin() {
         <h2>Login</h2>
 
         <div className='form-group'>
-          <label htmlFor='name'>Name:</label>
+          <label>Name:</label>
           <input type='text' onChange={(e)=> {setAdminId(e.target.value)}} />
         </div>
 
         <div className='form-group'>
-          <label htmlFor='password'>Password:</label>
+          <label>Password:</label>
           <input type='password' onChange={(e)=> {setPassword(e.target.value)}} />
         </div>
 
-        <button onClick={Login}>LOGIN</button>
+        <button type='submit' disabled={!validateForm()} onClick={Login}> Login </button>
 
-        <h1>{loginStatus}</h1>
       </div>
     </form>
   )
