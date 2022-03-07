ffcb635863f4df6e3e07dc243f835387c3658053
Jinwoo Jeong <qwaszxed1234@gmail.com>
2022-03-07 20:49:04 +0900

    admin login page merged

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { UserContextProvider } from "./components/Context/UserContext.js";
 import { AdminContextProvider } from "./components/Context/AdminContext.js";
 import InfoCheck from "./components/User/Page/InfoCheck.js";
 import UserMain from "./components/User/Page/UserMain.js";
+import AdminLogin from "./components/Admin/Page/AdminLogin.js";
 import AdminHome from "./components/Admin/Page/AdminHome.js";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -27,6 +28,12 @@ function App() {
             </UserContextProvider>
           }
         />
+        <Route
+          path="/admin/login"
+          element={
+            <AdminLogin />
+          }
+        ></Route>
         <Route
           path="/admin/home"
           element={
